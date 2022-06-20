aeb63ea1599211ca024895a202cef877e30a6c70
Артур Бичиковский <shiba.doingyoss@gmail.com>
2022-06-20 20:20:13 +0300

    html files, models added

--- src/musicapp/models.py
+++ src/musicapp/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
 class Track(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=50)
+    author = models.CharField(Author, on_delete=models.CASCADE)
     description = models.TextField()
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    band_name = models.CharField(max_length=50)
+
+
+class Albums(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    album_name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+
+class PlayList(models.Model):
+    playlist_name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    song = models.ForeignKey(Track, on_delete=models.CASCADE)
+
+
+class MusicLabel(models.Model):
+    label_name = models.CharField(max_length=100)
+    label_date = models.DateField()
--- src/musicapp/views.py
+++ src/musicapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Track
+
+
 # Create your views here.
 def index(request):
     tracklist = Track.objects.all()
     print(tracklist.query)
-    return render(request, 'index.html', {'tracklist':tracklist})
+    return render(request, 'index.html', {'tracklist': tracklist})
