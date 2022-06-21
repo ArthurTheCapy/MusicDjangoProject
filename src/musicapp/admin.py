0d5608a4d6b8264761cc721ec2839fc373b0d7d3
Артур Бичиковский <shiba.doingyoss@gmail.com>
2022-06-22 00:30:14 +0300

    update admin, models, css

--- src/musicapp/admin.py
+++ src/musicapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Track
+from .models import Track, Author, Album, PlayList, MusicLabel
 
 # Register your models here.
 admin.site.register(Track)
+admin.site.register(Author)
+admin.site.register(Album)
+admin.site.register(PlayList)
+admin.site.register(MusicLabel)
--- src/musicapp/models.py
+++ src/musicapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+# def user_directory_path(instance, filename):
+#    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
 # Create your models here.
 class Track(models.Model):
     title = models.CharField(max_length=50)
@@ -9,15 +13,16 @@ class Track(models.Model):
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
     band_name = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
 
 
 class Album(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     album_name = models.CharField(max_length=100)
-    release_date = models.DateField()
+    # album_picture = models.FileField(upload_to=user_directory_path)
+    album_release = models.CharField(max_length=100)
     num_stars = models.IntegerField()
 
 
@@ -29,4 +34,4 @@ class PlayList(models.Model):
 
 class MusicLabel(models.Model):
     label_name = models.CharField(max_length=100)
-    label_date = models.DateField()
+    label_description = models.CharField(max_length=100)
